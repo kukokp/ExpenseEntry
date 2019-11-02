192813ec0fb7dda4f76c875595bf9ff0d563f16e
krunal.prajapati <krunal.prajapati@vc-erp.com>
2019-11-02 12:01:32 +0530

    krunal

--- app/src/main/java/com/kepss/expenseentry/globle/Config.java
+++ app/src/main/java/com/kepss/expenseentry/globle/Config.java
@@ -5,6 +5,7 @@ public class Config {
     public static final String DATE_FORMAT = "dd-MM-yyyy";
     public static final String DATE_TIME_FORMAT = "dd-MM-yyyy HH:mm";
     public static final String TIME_FORMAT = "HH:mm";
+    public static final String sd = "HH:mm";
 
     // Config value
 
